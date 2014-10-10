138ffd4ea74bfc70ec9c506be670c6c1b9ba1aaa
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-10-10 13:27:14 -0700

    Bootstrap Xcode project

---
+++ Carthage/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Carthage
+//
+//  Created by Justin Spahr-Summers on 2014-10-10.
+//  Copyright (c) 2014 Carthage. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
