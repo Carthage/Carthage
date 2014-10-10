398e4d228d601cd28865daf40a27477df21180d5
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-10-10 14:58:48 -0700

    Add Comparable and Printable to Version

--- CarthageKit/Cartfile.swift
+++ CarthageKit/Cartfile.swift
@@ -13,10 +13,42 @@ public struct Dependency {
 	public var version: VersionSpecifier
 }
 
-public struct Version {
+public struct Version: Comparable {
 	public let major: Int
 	public let minor: Int
 	public let patch: Int
+}
+
+public func <(lhs: Version, rhs: Version) -> Bool {
+	if (lhs.major < rhs.major) {
+		return true
+	} else if (lhs.major > rhs.major) {
+		return false
+	}
+
+	if (lhs.minor < rhs.minor) {
+		return true
+	} else if (lhs.minor > rhs.minor) {
+		return false
+	}
+
+	if (lhs.patch < rhs.patch) {
+		return true
+	} else if (lhs.patch > rhs.patch) {
+		return false
+	}
+
+	return false
+}
+
+public func ==(lhs: Version, rhs: Version) -> Bool {
+	return lhs.major == rhs.major && lhs.minor == rhs.minor && lhs.patch == rhs.patch
+}
+
+extension Version: Printable {
+	public var description: String {
+		return "\(major).\(minor).\(patch)"
+	}
 }
 
 public enum VersionSpecifier {
