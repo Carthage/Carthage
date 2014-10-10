3c84b739aaa89fcd00acb4f8ce2b7bac1463a228
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-10-10 15:21:34 -0700

    Implement JSONDecodable for Repository

--- CarthageKit/GitHub.swift
+++ CarthageKit/GitHub.swift
@@ -7,8 +7,24 @@
 //
 
 import Foundation
+import LlamaKit
 
 public struct Repository {
 	public let owner: String
 	public let name: String
+}
+
+extension Repository: JSONDecodable {
+	public static func fromJSON(JSON: AnyObject) -> Result<Repository> {
+		if let NWO = JSON as? String {
+			let components = split(NWO, { $0 == "/" }, maxSplit: 1, allowEmptySlices: false)
+			if components.count < 2 {
+				return failure()
+			}
+
+			return success(self(owner: components[0], name: components[1]))
+		} else {
+			return failure()
+		}
+	}
 }
