4bfc174f3523a1ba483474f7c98ff0126a454074
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-10-10 15:45:57 -0700

    Add high-level JSON parsing function

--- CarthageKit/JSON.swift
+++ CarthageKit/JSON.swift
@@ -12,3 +12,18 @@ import LlamaKit
 internal protocol JSONDecodable {
 	class func fromJSON(JSON: AnyObject) -> Result<Self>
 }
+
+internal func parseJSONAtURL<T: JSONDecodable>(URL: NSURL) -> Result<T> {
+	var error: NSError?
+	if let data = NSData(contentsOfURL: URL, options: NSDataReadingOptions.allZeros, error: &error) {
+		if let object: AnyObject = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.allZeros, error: &error) {
+			return T.fromJSON(object)
+		} else if let error = error {
+			return failure(error)
+		}
+	} else if let error = error {
+		return failure(error)
+	}
+	
+	return failure()
+}
