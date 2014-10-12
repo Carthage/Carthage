a3e83a7c14aa2df00edda75e316ffcd1eabc39d7
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-10-11 22:02:43 -0700

    Draft project locating logic

--- CarthageKit/Xcode.swift
+++ CarthageKit/Xcode.swift
@@ -10,6 +10,123 @@ import Foundation
 import LlamaKit
 import ReactiveCocoa
 
+/// Describes how to locate the actual project or workspace that Xcode should
+/// build.
+public enum ProjectLocator: Comparable {
+	/// The `xcworkspace` at the given file URL should be built.
+	case Workspace(NSURL)
+
+	/// The `xcodeproj` at the given file URL should be built.
+	case ProjectFile(NSURL)
+
+	/// The file URL this locator refers to.
+	var fileURL: NSURL {
+		switch (self) {
+		case let .Workspace(URL):
+			assert(URL.fileURL)
+			return URL
+
+		case let .ProjectFile(URL):
+			assert(URL.fileURL)
+			return URL
+		}
+	}
+}
+
+public func ==(lhs: ProjectLocator, rhs: ProjectLocator) -> Bool {
+	switch (lhs, rhs) {
+	case let (.Workspace(left), .Workspace(right)):
+		return left == right
+
+	case let (.ProjectFile(left), .ProjectFile(right)):
+		return left == right
+
+	default:
+		return false
+	}
+}
+
+public func <(lhs: ProjectLocator, rhs: ProjectLocator) -> Bool {
+	// Prefer workspaces over projects.
+	switch (lhs, rhs) {
+	case let (.Workspace, .ProjectFile):
+		return true
+
+	case let (.ProjectFile, .Workspace):
+		return false
+
+	default:
+		return lexicographicalCompare(lhs.fileURL.path!, rhs.fileURL.path!)
+	}
+}
+
+/// A candidate match for a project's canonical `ProjectLocator`.
+private struct ProjectEnumerationMatch: Comparable {
+	let locator: ProjectLocator
+	let level: Int
+
+	/// Checks whether a project exists at the given URL, returning a match if
+	/// so.
+	static func matchURL(URL: NSURL, fromEnumerator enumerator: NSDirectoryEnumerator) -> Result<ProjectEnumerationMatch> {
+		var typeIdentifier: AnyObject?
+		var error: NSError?
+
+		if !URL.getResourceValue(&typeIdentifier, forKey: NSURLTypeIdentifierKey, error: &error) {
+			if let error = error {
+				return failure(error)
+			} else {
+				return failure()
+			}
+		}
+
+		if let typeIdentifier = typeIdentifier as? String {
+			if (UTTypeConformsTo(typeIdentifier, "com.apple.dt.document.workspace") != 0) {
+				return success(ProjectEnumerationMatch(locator: .Workspace(URL), level: enumerator.level))
+			} else if (UTTypeConformsTo(typeIdentifier, "com.apple.xcode.project") != 0) {
+				return success(ProjectEnumerationMatch(locator: .ProjectFile(URL), level: enumerator.level))
+			}
+		}
+
+		return failure()
+	}
+}
+
+private func ==(lhs: ProjectEnumerationMatch, rhs: ProjectEnumerationMatch) -> Bool {
+	return lhs.locator == rhs.locator
+}
+
+private func <(lhs: ProjectEnumerationMatch, rhs: ProjectEnumerationMatch) -> Bool {
+	if lhs.level < rhs.level {
+		return true
+	} else if lhs.level > rhs.level {
+		return false
+	}
+
+	return lhs.locator < rhs.locator
+}
+
+/// Attempts to locate a project or workspace within the given directory.
+public func locateProjectInDirectory(directoryURL: NSURL) -> Result<ProjectLocator> {
+	let enumerationOptions = NSDirectoryEnumerationOptions.SkipsSubdirectoryDescendants | NSDirectoryEnumerationOptions.SkipsHiddenFiles | NSDirectoryEnumerationOptions.SkipsPackageDescendants
+	let enumerator = NSFileManager.defaultManager().enumeratorAtURL(directoryURL, includingPropertiesForKeys: [ NSURLTypeIdentifierKey ], options: enumerationOptions, errorHandler: nil)
+	if let enumerator = enumerator {
+		var matches: [ProjectEnumerationMatch] = []
+
+		while let URL = enumerator.nextObject() as? NSURL {
+			if let match = ProjectEnumerationMatch.matchURL(URL, fromEnumerator: enumerator).value() {
+				matches.append(match)
+			}
+		}
+
+		if matches.count > 0 {
+			sort(&matches)
+			return success(matches[0].locator)
+		}
+	}
+
+	return failure()
+}
+
 public func buildInDirectory(directoryURL: NSURL, configuration: String = "Release") -> Promise<Result<()>> {
 	precondition(directoryURL.fileURL)
 
