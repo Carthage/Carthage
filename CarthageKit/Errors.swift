4fcd4e20eda9fc367f65600c53fe7be28d899132
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-10-24 13:35:12 -0700

    Add centralized Errors module

---
+++ CarthageKit/Errors.swift
@@ -0,0 +1,30 @@
+//
+//  Errors.swift
+//  Carthage
+//
+//  Created by Justin Spahr-Summers on 2014-10-24.
+//  Copyright (c) 2014 Carthage. All rights reserved.
+//
+
+import Foundation
+
+/// The domain for all errors originating within Carthage.
+public let CarthageErrorDomain: NSString = "org.carthage.Carthage"
+
+/// Possible error codes within `CarthageErrorDomain`.
+public enum CarthageError {
+	/// In a user info dictionary, associated with the exit code from a child
+	/// process.
+	static let exitCodeKey = "CarthageErrorExitCode"
+
+	/// A launched task failed with an erroneous exit code.
+	case ShellTaskFailed(status: Int)
+
+	/// An `NSError` object corresponding to this error code.
+	public var error: NSError {
+		switch (self) {
+		case let .ShellTaskFailed(code):
+			return NSError(domain: CarthageErrorDomain, code: 1, userInfo: [ CarthageError.exitCodeKey: code  ])
+		}
+	}
+}
