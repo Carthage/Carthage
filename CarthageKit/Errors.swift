f49fb40811fbec349ebd8c626eb1f3394c286c89
Alan Rogers <alanjrogers@me.com>
2014-10-26 21:43:20 +1100

    Add some more CarthageErrors

--- CarthageKit/Errors.swift
+++ CarthageKit/Errors.swift
@@ -20,11 +20,28 @@ public enum CarthageError {
 	/// A launched task failed with an erroneous exit code.
 	case ShellTaskFailed(exitCode: Int)
 
+	/// The git repository has already been cloned to the specified location
+	case RepositoryAlreadyCloned(location: String)
+
+	/// The git repository has a remote that doesn't match what we're trying to
+	/// clone
+	case RepositoryRemoteMismatch(expected: String, actual: String)
+
+	/// Unable to clone a git repository because a file with the same name
+	/// exists
+	case RepositoryCloneFailed(location: String)
+
 	/// An `NSError` object corresponding to this error code.
 	public var error: NSError {
 		switch (self) {
 		case let .ShellTaskFailed(code):
 			return NSError(domain: CarthageErrorDomain, code: 1, userInfo: [ CarthageError.exitCodeKey: code  ])
+		case let .RepositoryAlreadyCloned(location):
+			return NSError(domain: CarthageErrorDomain, code: 2, userInfo: [ NSLocalizedDescriptionKey: "The git repository already exists at \(location)."])
+		case let .RepositoryRemoteMismatch(expected, actual):
+			return NSError(domain: CarthageErrorDomain, code: 3, userInfo: [ NSLocalizedDescriptionKey: "Expected a remote URL: \(expected), but found \(actual)."])
+		case let .RepositoryCloneFailed(location):
+			return NSError(domain: CarthageErrorDomain, code: 4, userInfo: [ NSLocalizedDescriptionKey: "Unable to clone it repository because a file already exists at \(location)."])
 		}
 	}
 }
