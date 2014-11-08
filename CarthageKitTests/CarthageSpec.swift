52f58987e7e29b1fab90ab80985a40c44e660b6d
Alan Rogers <alanjrogers@me.com>
2014-11-08 18:33:05 +1100

    pew, pew.

--- CarthageKitTests/CarthageSpec.swift
+++ CarthageKitTests/CarthageSpec.swift
@@ -14,63 +14,62 @@ let CarthageSpecErrorDomain = "CarthageSpecErrorDomain"
 let CarthageSpecErrorUnzipFailed = -99
 
 class CarthageSpec: QuickSpec {
-    var tempDirectoryPath: NSString!
+	var tempDirectoryPath: NSString!
 
-    var repositoryFixturesPath: NSString {
-        if (tempDirectoryPath == nil) {
-            self.setUpTempDirectoryPath()
-        }
-        return self.tempDirectoryPath
-    }
+	var repositoryFixturesPath: NSString {
+		if (tempDirectoryPath == nil) {
+			self.setUpTempDirectoryPath()
+		}
+		return self.tempDirectoryPath
+	}
 
-    func setUpTempDirectoryPath() {
-        tempDirectoryPath = NSTemporaryDirectory().stringByAppendingPathComponent(NSUUID().UUIDString)
+	func setUpTempDirectoryPath() {
+		tempDirectoryPath = NSTemporaryDirectory().stringByAppendingPathComponent(NSUUID().UUIDString)
 
-        let fileManager = NSFileManager.defaultManager()
-        var error: NSError?
-        let success = fileManager.createDirectoryAtPath(tempDirectoryPath!, withIntermediateDirectories:true, attributes:nil, error:&error)
-        XCTAssertTrue(success, "Couldn't create the temp fixtures directory at \(tempDirectoryPath): \(error)")
-    }
+		let fileManager = NSFileManager.defaultManager()
+		var error: NSError?
+		let success = fileManager.createDirectoryAtPath(tempDirectoryPath!, withIntermediateDirectories:true, attributes:nil, error:&error)
+		XCTAssertTrue(success, "Couldn't create the temp fixtures directory at \(tempDirectoryPath): \(error)")
+	}
 
-    func setUpRepositoryFixtureIfNeeded(repositoryName: NSString) {
-        let path = self.repositoryFixturesPath.stringByAppendingPathComponent(repositoryName)
-        let fileManager = NSFileManager.defaultManager()
+	func setUpRepositoryFixtureIfNeeded(repositoryName: NSString) {
+		let path = self.repositoryFixturesPath.stringByAppendingPathComponent(repositoryName)
+		let fileManager = NSFileManager.defaultManager()
 
-        var isDirectory: ObjCBool = false
-        if (fileManager.fileExistsAtPath(path, isDirectory:&isDirectory) && isDirectory) {
-            return
-        }
+		var isDirectory: ObjCBool = false
+		if (fileManager.fileExistsAtPath(path, isDirectory:&isDirectory) && isDirectory) {
+			return
+		}
 
-        var error: NSError?
-        var success = fileManager.createDirectoryAtPath(self.repositoryFixturesPath, withIntermediateDirectories:true, attributes:nil, error:&error)
-        XCTAssertTrue(success, "Couldn't create the repository fixtures directory at \(self.repositoryFixturesPath): \(error)")
+		var error: NSError?
+		var success = fileManager.createDirectoryAtPath(self.repositoryFixturesPath, withIntermediateDirectories:true, attributes:nil, error:&error)
+		XCTAssertTrue(success, "Couldn't create the repository fixtures directory at \(self.repositoryFixturesPath): \(error)")
 
-        let zippedRepositoriesPath = NSBundle(forClass: self.dynamicType).resourcePath!.stringByAppendingPathComponent("fixtures").stringByAppendingPathComponent("repositories.zip")
+		let zippedRepositoriesPath = NSBundle(forClass: self.dynamicType).resourcePath!.stringByAppendingPathComponent("fixtures").stringByAppendingPathComponent("repositories.zip")
 
-        error = nil
-        success = unzipFile(repositoryName, zipPath:zippedRepositoriesPath, destinationPath:self.repositoryFixturesPath, error:&error)
-        XCTAssertTrue(success, "Couldn't unzip fixture \"\(repositoryName)\" from \(zippedRepositoriesPath) to \(self.repositoryFixturesPath): \(error)")
-    }
+		error = nil
+		success = unzipFile(repositoryName, zipPath:zippedRepositoriesPath, destinationPath:self.repositoryFixturesPath, error:&error)
+		XCTAssertTrue(success, "Couldn't unzip fixture \"\(repositoryName)\" from \(zippedRepositoriesPath) to \(self.repositoryFixturesPath): \(error)")
+	}
 
-    func pathForFixtureRepositoryNamed(repositoryName: String) -> NSURL {
-        setUpRepositoryFixtureIfNeeded(repositoryName)
-        return NSURL.fileURLWithPath("\(self.repositoryFixturesPath)/repositories/\(repositoryName)", isDirectory:true)!
-    }
+	func pathForFixtureRepositoryNamed(repositoryName: String) -> NSURL {
+		setUpRepositoryFixtureIfNeeded(repositoryName)
+		return NSURL.fileURLWithPath("\(self.repositoryFixturesPath)/repositories/\(repositoryName)", isDirectory:true)!
+	}
 
-    func unzipFile(member: NSString, zipPath: NSString, destinationPath: NSString, error: NSErrorPointer) -> Bool {
-        let task = NSTask()
-        task.launchPath = "/usr/bin/unzip"
-        task.arguments = [ "-qq", "-d", destinationPath, zipPath, "repositories/\(member)*", "-x", "*/.DS_Store" ]
+	func unzipFile(member: NSString, zipPath: NSString, destinationPath: NSString, error: NSErrorPointer) -> Bool {
+		let task = NSTask()
+		task.launchPath = "/usr/bin/unzip"
+		task.arguments = [ "-qq", "-d", destinationPath, zipPath, "repositories/\(member)*", "-x", "*/.DS_Store" ]
 
-        task.launch()
-        task.waitUntilExit()
+		task.launch()
+		task.waitUntilExit()
 
-        let success = (task.terminationStatus == 0)
-        if !success && error != nil {
-            error.memory = NSError(domain:CarthageSpecErrorDomain, code:CarthageSpecErrorUnzipFailed, userInfo:[ NSLocalizedDescriptionKey: "Unzip failed" ]);
-        }
+		let success = (task.terminationStatus == 0)
+		if !success && error != nil {
+			error.memory = NSError(domain:CarthageSpecErrorDomain, code:CarthageSpecErrorUnzipFailed, userInfo:[ NSLocalizedDescriptionKey: "Unzip failed" ]);
+		}
 
-        return success;
-    }
-
+		return success;
+	}
 }
