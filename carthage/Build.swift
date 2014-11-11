fa9f9f4de07d9e4e0d812050ddfe6ebf9f29fae4
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-11-11 11:47:18 -0800

    Factor out temporary file creation

--- carthage/Build.swift
+++ carthage/Build.swift
@@ -18,39 +18,50 @@ public struct BuildCommand: CommandType {
 	public func run(mode: CommandMode) -> Result<()> {
 		return ColdSignal.fromResult(BuildOptions.evaluate(mode))
 			.map { options -> ColdSignal<()> in
-				return ColdSignal.lazy {
-					let directoryURL = NSURL.fileURLWithPath(NSFileManager.defaultManager().currentDirectoryPath)!
+				return self.openTemporaryLogFile()
+					.map { (stdoutHandle, temporaryURL) -> ColdSignal<()> in
+						let directoryURL = NSURL.fileURLWithPath(NSFileManager.defaultManager().currentDirectoryPath)!
 
-					var temporaryDirectoryTemplate: ContiguousArray<CChar> = NSTemporaryDirectory().stringByAppendingPathComponent("carthage-xcodebuild.XXXXXX.log").nulTerminatedUTF8.map { CChar($0) }
-					let logFD = temporaryDirectoryTemplate.withUnsafeMutableBufferPointer { (inout template: UnsafeMutableBufferPointer<CChar>) -> Int32 in
-						return mkstemps(template.baseAddress, 4)
+						let (stdoutSignal, buildSignal) = buildInDirectory(directoryURL, withConfiguration: options.configuration, onlyScheme: options.scheme)
+						let disposable = stdoutSignal.observe { data in
+							stdoutHandle.writeData(data)
+						}
+
+						return buildSignal
+							.then(.empty())
+							.on(subscribed: {
+								println("xcodebuild output can be found in \(temporaryURL.path!)\n")
+							}, disposed: {
+								disposable.dispose()
+							})
 					}
-
-					if logFD < 0 {
-						return .error(NSError(domain: NSPOSIXErrorDomain, code: Int(errno), userInfo: nil))
-					}
-
-					let temporaryPath = temporaryDirectoryTemplate.withUnsafeBufferPointer { (ptr: UnsafeBufferPointer<CChar>) -> String in
-						return String.fromCString(ptr.baseAddress)!
-					}
-					
-					println("xcodebuild output can be found in \(temporaryPath)\n")
-
-					let stdoutHandle = NSFileHandle(fileDescriptor: logFD, closeOnDealloc: true)
-					let (stdoutSignal, buildSignal) = buildInDirectory(directoryURL, withConfiguration: options.configuration, onlyScheme: options.scheme)
-					let disposable = stdoutSignal.observe { data in
-						stdoutHandle.writeData(data)
-					}
-
-					return buildSignal
-						.then(.empty())
-						.on(disposed: {
-							disposable.dispose()
-						})
-				}
+					.merge(identity)
 			}
 			.merge(identity)
 			.wait()
+	}
+
+	/// Opens a temporary file for logging, returning the handle and the URL to
+	/// the file on disk.
+	private func openTemporaryLogFile() -> ColdSignal<(NSFileHandle, NSURL)> {
+		return ColdSignal.lazy {
+			var temporaryDirectoryTemplate: ContiguousArray<CChar> = NSTemporaryDirectory().stringByAppendingPathComponent("carthage-xcodebuild.XXXXXX.log").nulTerminatedUTF8.map { CChar($0) }
+			let logFD = temporaryDirectoryTemplate.withUnsafeMutableBufferPointer { (inout template: UnsafeMutableBufferPointer<CChar>) -> Int32 in
+				return mkstemps(template.baseAddress, 4)
+			}
+
+			if logFD < 0 {
+				return .error(NSError(domain: NSPOSIXErrorDomain, code: Int(errno), userInfo: nil))
+			}
+
+			let temporaryPath = temporaryDirectoryTemplate.withUnsafeBufferPointer { (ptr: UnsafeBufferPointer<CChar>) -> String in
+				return String.fromCString(ptr.baseAddress)!
+			}
+
+			let stdoutHandle = NSFileHandle(fileDescriptor: logFD, closeOnDealloc: true)
+			let temporaryURL = NSURL.fileURLWithPath(temporaryPath, isDirectory: false)!
+			return .single((stdoutHandle, temporaryURL))
+		}
 	}
 }
 
