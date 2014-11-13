b46733d5edbbce7257618c7723e57d463e29df9d
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-11-12 21:32:29 -0800

    Check whether fetching or cloning at Project level

--- CarthageKit/Project.swift
+++ CarthageKit/Project.swift
@@ -10,7 +10,8 @@ import Foundation
 import LlamaKit
 import ReactiveCocoa
 
-let dependenciesURL = NSURL.fileURLWithPath("~/.carthage/dependencies".stringByExpandingTildeInPath, isDirectory:true)!
+/// The file URL to the directory in which cloned dependencies will be stored.
+public let CarthageDependencyRepositoriesURL = NSURL.fileURLWithPath("~/.carthage/dependencies".stringByExpandingTildeInPath, isDirectory:true)!
 
 /// Represents a Project that is using Carthage.
 public struct Project {
@@ -39,21 +40,29 @@ public struct Project {
 /// Checks out the dependencies listed in the project's Cartfile.
 public func checkoutProjectDependencies(project: Project) -> ColdSignal<()> {
 	return ColdSignal.fromValues(project.cartfile.dependencies)
-		.map({ dependency -> ColdSignal<String> in
+		.map { dependency -> ColdSignal<String> in
 			switch dependency.project {
 			case let .GitHub(repository):
-				let destinationURL = dependenciesURL.URLByAppendingPathComponent("\(repository.name)")
+				let destinationURL = CarthageDependencyRepositoriesURL.URLByAppendingPathComponent(repository.name)
 
-				return cloneRepository(repository.cloneURL.absoluteString!, destinationURL)
-					.catch( {error in
-						println(error.localizedDescription)
-						if error.code == CarthageError.RepositoryAlreadyCloned(location: destinationURL).error.code {
-							return fetchRepository(destinationURL).catch { _ in return .empty() }
-						}
-						return ColdSignal.empty()
-					})
+				var isDirectory: ObjCBool = false
+				if NSFileManager.defaultManager().fileExistsAtPath(destinationURL.path!, isDirectory: &isDirectory) {
+					return fetchRepository(destinationURL)
+						.on(subscribed: {
+							println("*** Fetching \(dependency.project.name)")
+						}, terminated: {
+							println()
+						})
+				} else {
+					return cloneRepository(repository.cloneURLString, destinationURL)
+						.on(subscribed: {
+							println("*** Cloning \(dependency.project.name)")
+						}, terminated: {
+							println()
+						})
+				}
 			}
-		})
+		}
 		.concat(identity)
 		.then(.empty())
 }
