2f33e73ae3da4d2ad0567c4efcc1b661730bc960
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-11-20 11:50:02 -0800

    Refactor `update` command to use `checkout`

--- carthage/Checkout.swift
+++ carthage/Checkout.swift
@@ -17,33 +17,44 @@ public struct CheckoutCommand: CommandType {
 
 	public func run(mode: CommandMode) -> Result<()> {
 		return ColdSignal.fromResult(CheckoutOptions.evaluate(mode))
-			.map { options -> ColdSignal<()> in
-				let directoryURL = NSURL.fileURLWithPath(options.directoryPath, isDirectory: true)!
-
-				return ColdSignal.fromResult(Project.loadFromDirectory(directoryURL))
-					.on(next: { project in
-						project.preferHTTPS = !options.useSSH
-						project.projectEvents.observe(ProjectEventSink())
-					})
-					.map { $0.checkoutLockedDependencies() }
-					.merge(identity)
-			}
+			.map { self.checkoutWithOptions($0) }
 			.merge(identity)
 			.wait()
 	}
+
+	/// Checks out dependencies with the given options.
+	public func checkoutWithOptions(options: CheckoutOptions) -> ColdSignal<()> {
+		return ColdSignal.fromResult(options.loadProject())
+			.map { $0.checkoutLockedDependencies() }
+			.merge(identity)
+	}
 }
 
-private struct CheckoutOptions: OptionsType {
-	let directoryPath: String
-	let useSSH: Bool
+public struct CheckoutOptions: OptionsType {
+	public let directoryPath: String
+	public let useSSH: Bool
 
-	static func create(useSSH: Bool)(directoryPath: String) -> CheckoutOptions {
+	public static func create(useSSH: Bool)(directoryPath: String) -> CheckoutOptions {
 		return self(directoryPath: directoryPath, useSSH: useSSH)
 	}
 
-	static func evaluate(m: CommandMode) -> Result<CheckoutOptions> {
+	public static func evaluate(m: CommandMode) -> Result<CheckoutOptions> {
 		return create
 			<*> m <| Option(key: "use-ssh", defaultValue: false, usage: "whether to use SSH for GitHub repositories")
 			<*> m <| Option(defaultValue: NSFileManager.defaultManager().currentDirectoryPath, usage: "the directory containing the Carthage project")
+	}
+
+	/// Attempts to load the project referenced by the options, and configure it
+	/// accordingly.
+	public func loadProject() -> Result<Project> {
+		if let directoryURL = NSURL.fileURLWithPath(self.directoryPath, isDirectory: true) {
+			return Project.loadFromDirectory(directoryURL).map { project in
+				project.preferHTTPS = !self.useSSH
+				project.projectEvents.observe(ProjectEventSink())
+				return project
+			}
+		} else {
+			return failure(CarthageError.InvalidArgument(description: "Invalid project path: \(directoryPath)").error)
+		}
 	}
 }
--- carthage/Update.swift
+++ carthage/Update.swift
@@ -18,18 +18,12 @@ public struct UpdateCommand: CommandType {
 	public func run(mode: CommandMode) -> Result<()> {
 		return ColdSignal.fromResult(UpdateOptions.evaluate(mode))
 			.map { options -> ColdSignal<()> in
-				let directoryURL = NSURL.fileURLWithPath(options.directoryPath, isDirectory: true)!
-
 				var buildSignal: ColdSignal<()> = .empty()
 				if options.buildAfterUpdate {
-					buildSignal = BuildCommand().buildWithOptions(BuildOptions(configuration: options.configuration, skipCurrent: true, directoryPath: options.directoryPath))
+					buildSignal = BuildCommand().buildWithOptions(BuildOptions(configuration: options.configuration, skipCurrent: true, directoryPath: options.checkoutOptions.directoryPath))
 				}
 
-				return ColdSignal.fromResult(Project.loadFromDirectory(directoryURL))
-					.on(next: { project in
-						project.preferHTTPS = !options.useSSH
-						project.projectEvents.observe(ProjectEventSink())
-					})
+				return ColdSignal.fromResult(options.checkoutOptions.loadProject())
 					.map { $0.updateDependencies() }
 					.merge(identity)
 					.then(buildSignal)
@@ -42,18 +36,16 @@ public struct UpdateCommand: CommandType {
 private struct UpdateOptions: OptionsType {
 	let buildAfterUpdate: Bool
 	let configuration: String
-	let directoryPath: String
-	let useSSH: Bool
+	let checkoutOptions: CheckoutOptions
 
-	static func create(configuration: String)(buildAfterUpdate: Bool)(useSSH: Bool)(directoryPath: String) -> UpdateOptions {
-		return self(buildAfterUpdate: buildAfterUpdate, configuration: configuration, directoryPath: directoryPath, useSSH: useSSH)
+	static func create(configuration: String)(buildAfterUpdate: Bool)(checkoutOptions: CheckoutOptions) -> UpdateOptions {
+		return self(buildAfterUpdate: buildAfterUpdate, configuration: configuration, checkoutOptions: checkoutOptions)
 	}
 
 	static func evaluate(m: CommandMode) -> Result<UpdateOptions> {
 		return create
 			<*> m <| Option(key: "configuration", defaultValue: "Release", usage: "the Xcode configuration to build (if --build is enabled)")
 			<*> m <| Option(key: "build", defaultValue: true, usage: "whether to build dependencies after updating")
-			<*> m <| Option(key: "use-ssh", defaultValue: false, usage: "whether to use SSH for GitHub repositories")
-			<*> m <| Option(defaultValue: NSFileManager.defaultManager().currentDirectoryPath, usage: "the directory containing the Carthage project")
+			<*> CheckoutOptions.evaluate(m)
 	}
 }
