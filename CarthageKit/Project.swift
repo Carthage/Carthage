6e529509da1c9c8686f755a38c3f2e2e1eca88c3
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-11-21 11:52:35 -0800

    Revert "Ignore Carthage.checkout folders in submodules"
    This reverts commit 59a405d10e57b1e8304a4ceb0e7e8f84e3075719.

--- CarthageKit/Project.swift
+++ CarthageKit/Project.swift
@@ -296,17 +296,7 @@ public final class Project {
 				if let submodule = submodule {
 					return self.runGitOperation(addSubmoduleToRepository(self.directoryURL, submodule, GitURL(repositoryURL.path!)))
 				} else {
-					return checkoutRepositoryToDirectory(repositoryURL, workingDirectoryURL, revision: revision) { submodule -> Bool in
-						// Ignore submodules that correspond to Carthage
-						// checkouts _in_ this dependency.
-						if let firstComponent = submodule.path.pathComponents.first {
-							if firstComponent == CarthageProjectCheckoutsPath {
-								return false
-							}
-						}
-
-						return true
-					}
+					return checkoutRepositoryToDirectory(repositoryURL, workingDirectoryURL, revision: revision)
 				}
 			}
 			.on(subscribed: {
