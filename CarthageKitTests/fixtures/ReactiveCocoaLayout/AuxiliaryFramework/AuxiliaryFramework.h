8ee75e636f9dc7a3bf6fd60b75690b27bf111340
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-11-26 14:53:38 -0600

    Add empty framework built with RCL iOS scheme
    Used to test that all framework targets in a scheme are built.

---
+++ CarthageKitTests/fixtures/ReactiveCocoaLayout/AuxiliaryFramework/AuxiliaryFramework.h
@@ -0,0 +1,19 @@
+//
+//  AuxiliaryFramework.h
+//  AuxiliaryFramework
+//
+//  Created by Justin Spahr-Summers on 2014-11-26.
+//  Copyright (c) 2014 GitHub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AuxiliaryFramework.
+FOUNDATION_EXPORT double AuxiliaryFrameworkVersionNumber;
+
+//! Project version string for AuxiliaryFramework.
+FOUNDATION_EXPORT const unsigned char AuxiliaryFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AuxiliaryFramework/PublicHeader.h>
+
+
