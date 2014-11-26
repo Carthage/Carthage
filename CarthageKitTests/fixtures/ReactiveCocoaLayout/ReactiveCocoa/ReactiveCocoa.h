76182cbed28709441aabc02e5e68d1ba2e17c7a0
Justin Spahr-Summers <jspahrsummers@github.com>
2014-11-26 17:04:46 -0600

    Merge pull request #141 from Carthage/only-build-frameworks
    Only build framework schemes

---
+++ CarthageKitTests/fixtures/ReactiveCocoaLayout/ReactiveCocoa/ReactiveCocoa.h
@@ -0,0 +1,13 @@
+//
+//  ReactiveCocoa.h
+//  ReactiveCocoa
+//
+//  Created by Justin Spahr-Summers on 2014-11-25.
+//  Copyright (c) 2014 GitHub. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ReactiveCocoa : NSObject
+
+@end
