a58ab3e6c823de8aae8df59f40723df60583a210
Robert Böhnke <robb@robb.is>
2014-12-27 17:53:11 +0100

    Add a regression test for loading a lone Cartfile.private

---
+++ Source/CarthageKitTests/ProjectSpec.swift
@@ -0,0 +1,29 @@
+//
+//  ProjectSpec.swift
+//  Carthage
+//
+//  Created by Robert Böhnke on 27/12/14.
+//  Copyright (c) 2014 Carthage. All rights reserved.
+//
+
+import CarthageKit
+import Foundation
+import Nimble
+import Quick
+
+class ProjectSpec: QuickSpec {
+	override func spec() {
+		let directoryURL = NSBundle(forClass: self.dynamicType).URLForResource("CartfilePrivateOnly", withExtension: nil)!
+
+		it("should load a combined Cartfile when only a Cartfile.private is present") {
+			let result = Project.loadCombinedCartfile(directoryURL).single()
+
+			expect(result.isSuccess()).to(beTrue())
+
+			let dependencies = result.value()!.dependencies
+
+			expect(countElements(dependencies)).to(equal(1))
+			expect(dependencies[0].project.name).to(equal("Carthage"))
+		}
+	}
+}
