170df40845e0cfcc84b952df5ff1876e909ad9a9
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2015-03-21 13:17:01 -0700

    Update `archive` command

--- Source/carthage/Archive.swift
+++ Source/carthage/Archive.swift
@@ -16,32 +16,33 @@ public struct ArchiveCommand: CommandType {
 	public let verb = "archive"
 	public let function = "Archives a built framework into a zip that Carthage can use"
 
-	public func run(mode: CommandMode) -> Result<(), CommandantError> {
-		return ColdSignal.fromResult(ArchiveOptions.evaluate(mode))
-			.mergeMap { options -> ColdSignal<()> in
+	public func run(mode: CommandMode) -> Result<(), CommandantError<CarthageError>> {
+		return producerWithOptions(ArchiveOptions.evaluate(mode))
+			|> joinMap(.Merge) { options -> SignalProducer<(), CommandError> in
 				let formatting = options.colorOptions.formatting
 
-				return ColdSignal.fromValues(Platform.supportedPlatforms)
-					.map { platform in platform.relativePath.stringByAppendingPathComponent(options.frameworkName).stringByAppendingPathExtension("framework")! }
-					.filter { relativePath in NSFileManager.defaultManager().fileExistsAtPath(relativePath) }
-					.on(next: { path in
+				return SignalProducer(values: Platform.supportedPlatforms)
+					|> map { platform in platform.relativePath.stringByAppendingPathComponent(options.frameworkName).stringByAppendingPathExtension("framework")! }
+					|> filter { relativePath in NSFileManager.defaultManager().fileExistsAtPath(relativePath) }
+					|> on(next: { path in
 						carthage.println(formatting.bullets + "Found " + formatting.path(string: path))
 					})
-					.reduce(initial: []) { $0 + [ $1 ] }
-					.mergeMap { paths -> ColdSignal<()> in
+					|> reduce([]) { $0 + [ $1 ] }
+					|> joinMap(.Merge) { paths -> SignalProducer<(), CarthageError> in
 						if paths.isEmpty {
-							return .error(CarthageError.InvalidArgument(description: "Could not find any copies of \(options.frameworkName).framework. Make sure you're in the project’s root and that the framework has already been built.").error)
+							return SignalProducer(error: CarthageError.InvalidArgument(description: "Could not find any copies of \(options.frameworkName).framework. Make sure you're in the project’s root and that the framework has already been built."))
 						}
 
 						let outputPath = (options.outputPath.isEmpty ? "\(options.frameworkName).framework.zip" : options.outputPath)
 						let outputURL = NSURL(fileURLWithPath: outputPath, isDirectory: false)!
 
-						return zipIntoArchive(outputURL, paths).on(completed: {
+						return zipIntoArchive(outputURL, paths) |> on(completed: {
 							carthage.println(formatting.bullets + "Created " + formatting.path(string: outputPath))
 						})
 					}
+					|> promoteErrors
 			}
-			.wait()
+			|> waitOnCommand
 	}
 }
 
@@ -54,7 +55,7 @@ private struct ArchiveOptions: OptionsType {
 		return self(frameworkName: frameworkName, outputPath: outputPath, colorOptions: colorOptions)
 	}
 
-	static func evaluate(m: CommandMode) -> Result<ArchiveOptions, CommandantError> {
+	static func evaluate(m: CommandMode) -> Result<ArchiveOptions, CommandantError<CarthageError>> {
 		return create
 			<*> m <| Option(key: "output", defaultValue: "", usage: "the path at which to create the zip file (or blank to infer it from the framework name)")
 			<*> ColorOptions.evaluate(m)
