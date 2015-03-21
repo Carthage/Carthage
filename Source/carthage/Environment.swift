6ecec68d16f10653415599cc396f74895f4e30eb
Rob Rix <rob.rix@github.com>
2015-03-20 21:49:20 -0400

    as?

--- Source/carthage/Environment.swift
+++ Source/carthage/Environment.swift
@@ -13,7 +13,7 @@ import LlamaKit
 internal func getEnvironmentVariable(variable: String) -> Result<String> {
 	let environment = NSProcessInfo.processInfo().environment
 
-	if let value = environment[variable] as String? {
+	if let value = environment[variable] as? String {
 		return success(value)
 	} else {
 		return failure(CarthageError.MissingEnvironmentVariable(variable: variable).error)
