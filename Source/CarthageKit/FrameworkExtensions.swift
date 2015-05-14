7b2e6eae7b0374ec9ea9f875d606d1af4ee7583e
Rob Rix <rob.rix@github.com>
2015-05-14 19:35:42 -0400

    Interrupt more politely.

--- Source/CarthageKit/FrameworkExtensions.swift
+++ Source/CarthageKit/FrameworkExtensions.swift
@@ -48,8 +48,10 @@ internal func permuteWith<T, U, E>(otherSignal: Signal<U, E>)(signal: Signal<T, 
 
 		var signalValues: [T] = []
 		var signalCompleted = false
+		var signalInterrupted = false
 		var otherValues: [U] = []
 		var otherCompleted = false
+		var otherInterrupted = false
 
 		let signalDisposable = signal.observe(next: { value in
 			lock.lock()
@@ -72,7 +74,14 @@ internal func permuteWith<T, U, E>(otherSignal: Signal<U, E>)(signal: Signal<T, 
 
 			lock.unlock()
 		}, interrupted: {
-			sendInterrupted(observer)
+			lock.lock()
+
+			signalInterrupted = true
+			if otherInterrupted || otherCompleted {
+				sendInterrupted(observer)
+			}
+
+			lock.unlock()
 		})
 
 		let otherDisposable = otherSignal.observe(next: { value in
@@ -96,7 +105,14 @@ internal func permuteWith<T, U, E>(otherSignal: Signal<U, E>)(signal: Signal<T, 
 
 			lock.unlock()
 		}, interrupted: {
-			sendInterrupted(observer)
+			lock.lock()
+
+			otherInterrupted = true
+			if signalInterrupted || signalCompleted {
+				sendInterrupted(observer)
+			}
+
+			lock.unlock()
 		})
 
 		let compositeDisposable = CompositeDisposable()
