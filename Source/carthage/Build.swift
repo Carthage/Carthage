f85058d0bbef136d1a7a912b859e1329b732bc54
Javier Soto <javier.api@gmail.com>
2015-06-12 19:10:09 -0700

    Correct mapping for "watchsimulator"

--- Source/carthage/Build.swift
+++ Source/carthage/Build.swift
@@ -270,7 +270,7 @@ extension BuildPlatform: ArgumentType {
 	private static let acceptedStrings: [String: BuildPlatform] = [
 		"Mac": .Mac, "macosx": .Mac,
 		"iOS": .iOS, "iphoneos": .iOS, "iphonesimulator": .iOS,
-		"watchOS": .watchOS, "watchos": .watchOS, "whatchsimulator": .iOS,
+		"watchOS": .watchOS, "watchos": .watchOS, "whatchsimulator": .watchOS,
 		"all": .All
 	]
 
