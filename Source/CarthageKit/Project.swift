537c777b15273aecc5881b6af5ee79f3cd20ea0c
Matt Diephouse <matt@diephouse.com>
2016-02-06 14:56:37 -0500

    Don't use a localized sort
    Otherwise build order will depend on your localization.

--- Source/CarthageKit/Project.swift
+++ Source/CarthageKit/Project.swift
@@ -564,7 +564,7 @@ public final class Project {
 					if let deps = graph[left.project] where deps.contains(right.project) {
 						return false
 					}
-					return left.project.name.localizedStandardCompare(right.project.name) == .OrderedAscending
+					return left.project.name.caseInsensitiveCompare(right.project.name) == .OrderedAscending
 				}
 				return SignalProducer(values: sorted)
 			}
