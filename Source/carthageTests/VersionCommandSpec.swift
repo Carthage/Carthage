15b9c255a2c9efccf8e4cd5d16e43440295bdfcf
Rachel Brindle <you@subluminal.net>
2016-02-11 13:28:27 -0800

    Change version command spec to be more integration-y - describing the behavior we actually want

--- Source/carthageTests/VersionCommandSpec.swift
+++ Source/carthageTests/VersionCommandSpec.swift
@@ -2,20 +2,43 @@ import Quick
 import Nimble
 import CarthageKit
 import Commandant
+import Cocoa
 
 import carthage
 
 private class MockPrinter: Printer {
 	private func println() {}
 
-	private var objectToPrint: Any?
+	private var objectToPrintln: String?
 	private func println(object: Any) {
-		objectToPrint = object
+		if let description = (object as? CustomStringConvertible)?.description {
+			objectToPrintln = description
+		}
 	}
 
+	private var objectToPrint: String?
 	private func print(object: Any) {
-		objectToPrint = object
+		if let description = (object as? CustomStringConvertible)?.description {
+			objectToPrint = description
+		}
 	}
+}
+
+// Because this by-default synchronous and way easier to understand than ReactiveTask
+private func runCommand(command: String, fromDirectory: String = NSProcessInfo.processInfo().environment["SourceDir"] ?? "") -> String {
+	let task = NSTask()
+	let arguments = command.componentsSeparatedByString(" ")
+	task.launchPath = arguments.first!
+	task.arguments = Array(arguments[1..<arguments.count])
+	task.currentDirectoryPath = fromDirectory
+
+	let pipe = NSPipe()
+	task.standardOutput = pipe
+	task.standardError = pipe
+	task.launch()
+	task.waitUntilExit()
+
+	return String(data: pipe.fileHandleForReading.readDataToEndOfFile(), encoding: NSUTF8StringEncoding) ?? ""
 }
 
 class VersionSpec: QuickSpec {
@@ -28,14 +51,12 @@ class VersionSpec: QuickSpec {
 			subject = VersionCommand(printer: printer)
 		}
 
-		it("prints the latest CFBundleShortVersionString from CarthageKit") {
-			let versionString = NSBundle(identifier: CarthageKitBundleIdentifier)?.objectForInfoDictionaryKey("CFBundleShortVersionString") as! String
+		it("prints the output of git describe --tags on the carthage directory as the version") {
+			let versionString = runCommand("/usr/bin/git describe --tags")
 
-			// expect(subject.run(NoOptions<CarthageError>().error).to(beNil())
-			// Waiting for https://github.com/Carthage/Commandant/pull/55 to be merged
-			// and for Carthage to update
+//			subject.run(NoOptions<CarthageError>())
 
-			expect(printer.objectToPrint as? String) == versionString
+			expect(printer.objectToPrintln) == versionString
 		}
     }
 }
