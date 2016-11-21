361a178e11f19b8dc01138d4726cb81f45490d4c
Syo Ikeda <suicaicoca@gmail.com>
2016-11-21 23:43:28 +0900

    [Availability] Remove unnecessary Swift version condition

--- Source/CarthageKit/Availability.swift
+++ Source/CarthageKit/Availability.swift
@@ -1,16 +1,13 @@
-#if swift(>=3)
-#else
-	import Foundation
-	import ReactiveCocoa
+import Foundation
+import ReactiveCocoa
 
-	// MARK: - Archive.swift
+// MARK: - Archive.swift
 
-	@available(*, unavailable, renamed="zip(paths:into:workingDirectory:)")
-	public func zipIntoArchive(destinationArchiveURL: NSURL, workingDirectory: String, inputPaths: [String]) -> SignalProducer<(), CarthageError> { fatalError() }
+@available(*, unavailable, renamed="zip(paths:into:workingDirectory:)")
+public func zipIntoArchive(destinationArchiveURL: NSURL, workingDirectory: String, inputPaths: [String]) -> SignalProducer<(), CarthageError> { fatalError() }
 
-	@available(*, unavailable, renamed="unzip(archive:to:)")
-	public func unzipArchiveToDirectory(fileURL: NSURL, _ destinationDirectoryURL: NSURL) -> SignalProducer<(), CarthageError> { fatalError() }
+@available(*, unavailable, renamed="unzip(archive:to:)")
+public func unzipArchiveToDirectory(fileURL: NSURL, _ destinationDirectoryURL: NSURL) -> SignalProducer<(), CarthageError> { fatalError() }
 
-	@available(*, unavailable, renamed="unzip(archive:)")
-	public func unzipArchiveToTemporaryDirectory(fileURL: NSURL) -> SignalProducer<NSURL, CarthageError> { fatalError() }
-#endif
+@available(*, unavailable, renamed="unzip(archive:)")
+public func unzipArchiveToTemporaryDirectory(fileURL: NSURL) -> SignalProducer<NSURL, CarthageError> { fatalError() }
