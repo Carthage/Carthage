6ac29eff49bcf593639e17807e4341b27154e42b
Syo Ikeda <suicaicoca@gmail.com>
2017-01-07 17:43:33 +0900

    Make `schemesInProject` an instance method of ProjectLocator

--- Source/CarthageKit/ProjectLocator.swift
+++ Source/CarthageKit/ProjectLocator.swift
@@ -1,4 +1,5 @@
 import Foundation
+import ReactiveCocoa
 
 /// Describes how to locate the actual project or workspace that Xcode should
 /// build.
@@ -25,6 +26,41 @@ public enum ProjectLocator: Comparable {
 	/// The number of levels deep the current object is in the directory hierarchy.
 	public var level: Int {
 		return fileURL.carthage_pathComponents.count - 1
+	}
+
+	/// Sends each scheme found in the receiver.
+	public func schemes() -> SignalProducer<String, CarthageError> {
+		let task = xcodebuildTask("-list", BuildArguments(project: self))
+
+		return task.launch()
+			.ignoreTaskData()
+			.mapError(CarthageError.taskError)
+			// xcodebuild has a bug where xcodebuild -list can sometimes hang
+			// indefinitely on projects that don't share any schemes, so
+			// automatically bail out if it looks like that's happening.
+			.timeout(after: 60, raising: .xcodebuildTimeout(self), on: QueueScheduler(qos: QOS_CLASS_DEFAULT))
+			.retry(upTo: 2)
+			.map { data in
+				return String(data: data, encoding: .utf8)!
+			}
+			.flatMap(.merge) { string in
+				return string.linesProducer
+			}
+			.flatMap(.merge) { line -> SignalProducer<String, CarthageError> in
+				// Matches one of these two possible messages:
+				//
+				// '    This project contains no schemes.'
+				// 'There are no schemes in workspace "Carthage".'
+				if line.hasSuffix("contains no schemes.") || line.hasPrefix("There are no schemes") {
+					return SignalProducer(error: .noSharedSchemes(self, nil))
+				} else {
+					return SignalProducer(value: line)
+				}
+			}
+			.skip { line in !line.hasSuffix("Schemes:") }
+			.skip(first: 1)
+			.take { line in !line.isEmpty }
+			.map { line in line.trimmingCharacters(in: .whitespaces) }
 	}
 }
 
