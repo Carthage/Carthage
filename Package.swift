bcf94d398afd3028ddd3ef87c9fc831b66ddfeb0
Syo Ikeda <suicaicoca@gmail.com>
2017-02-08 09:24:11 +0900

    [SwiftPM] Add Package.swift and .swift-version

---
+++ Package.swift
@@ -0,0 +1,27 @@
+import PackageDescription
+
+let package = Package(
+    name: "Carthage",
+    targets: [
+        Target(name: "XCDBLD"),
+        Target(name: "CarthageKit", dependencies: [ "XCDBLD" ]),
+        Target(name: "carthage", dependencies: [ "XCDBLD", "CarthageKit" ]),
+    ],
+    dependencies: [
+        .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 3, minor: 2),
+        .Package(url: "https://github.com/Carthage/ReactiveTask.git", versions: Version(0, 11, 1)..<Version(0, 11, .max)),
+        .Package(url: "https://github.com/Carthage/Commandant.git", majorVersion: 0, minor: 12),
+        .Package(url: "https://github.com/jdhealy/PrettyColors.git", majorVersion: 5),
+        .Package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/mdiep/Tentacle.git", majorVersion: 0, minor: 6),
+        .Package(url: "https://github.com/thoughtbot/Argo.git", majorVersion: 4, minor: 1),
+        .Package(url: "https://github.com/thoughtbot/Runes.git", versions: Version(4, 0, 1)..<Version(4, .max, .max)),
+    ],
+    exclude: [
+        "Source/CarthageIntegration",
+        "Source/CarthageKitTests",
+        "Source/Scripts",
+        "Source/XCDBLDTests",
+        "Source/carthage/swift-is-crashy.c",
+    ]
+)
