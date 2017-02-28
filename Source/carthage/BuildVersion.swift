c31b9e1178d0ba768d7a0f756e1e44723f438cf6
Syo Ikeda <suicaicoca@gmail.com>
2017-02-28 13:39:07 +0900

    Ignore draft releases for the new Carthage release check

--- Source/carthage/BuildVersion.swift
+++ Source/carthage/BuildVersion.swift
@@ -21,7 +21,9 @@ public func localVersion() -> SemanticVersion {
 public func remoteVersion() -> SemanticVersion? {
 	let latestRemoteVersion = Client(.dotCom)
 		.releases(in: Repository(owner: "Carthage", name: "Carthage"), perPage: 1)
-		.map { _, releases in releases.first! }
+		.map { _, releases in
+			return releases.first { !$0.isDraft }!
+		}
 		.mapError(CarthageError.gitHubAPIRequestFailed)
 		.attemptMap { release -> Result<SemanticVersion, CarthageError> in
 			return SemanticVersion.from(Scanner(string: release.tag)).mapError(CarthageError.init(scannableError:))
