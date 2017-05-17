8117f2e7f370fbe214f734458858420833174ee4
Syo Ikeda <suicaicoca@gmail.com>
2017-05-17 13:50:41 +0900

    Support building static frameworks

--- Source/CarthageKit/Xcode.swift
+++ Source/CarthageKit/Xcode.swift
@@ -308,7 +308,7 @@ private func mergeModuleIntoModule(_ sourceModuleDirectoryURL: URL, _ destinatio
 
 /// Determines whether the specified framework type should be built automatically.
 private func shouldBuildFrameworkType(_ frameworkType: FrameworkType?) -> Bool {
-	return frameworkType == .dynamic
+	return frameworkType != nil
 }
 
 /// Determines whether the given scheme should be built automatically.
@@ -330,7 +330,7 @@ private func shouldBuildScheme(_ buildArguments: BuildArguments, _ forPlatforms:
 			}
 		}
 		.filter(shouldBuildFrameworkType)
-		// If we find any dynamic framework target, we should indeed build this scheme.
+		// If we find any framework target, we should indeed build this scheme.
 		.map { _ in true }
 		// Otherwise, nope.
 		.concat(value: false)
@@ -644,7 +644,7 @@ private func build(sdk: SDK, with buildArgs: BuildArguments, in workingDirectory
 			let xcodebuildAction: BuildArguments.Action = sdk.isDevice ? .archive : .build
 			return BuildSettings.load(with: argsForLoading, for: xcodebuildAction)
 				.filter { settings in
-					// Only copy build products that are dynamic frameworks
+					// Only copy build products that are frameworks
 					guard let frameworkType = settings.frameworkType.value, shouldBuildFrameworkType(frameworkType), let projectPath = settings.projectPath.value else {
 						return false
 					}
