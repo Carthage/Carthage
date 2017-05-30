8af642e061c82033aa481451774e19543e063405
Syo Ikeda <suicaicoca@gmail.com>
2017-05-31 02:05:44 +0900

    [CI] Run `swift test`
    Since SwiftPM creates a test bundle on macOS,
    
    - Let SwiftPM just build the test bundle without running it
    - Copy test resources into the bundle
    - Finally run the test bundle

--- Package.swift
+++ Package.swift
@@ -1,4 +1,9 @@
+import Foundation
 import PackageDescription
+
+var isSwiftPackagerManagerTest: Bool {
+    return ProcessInfo.processInfo.environment["SWIFTPM_TEST_Carthage"] == "YES"
+}
 
 let package = Package(
     name: "Carthage",
@@ -7,16 +12,25 @@ let package = Package(
         Target(name: "CarthageKit", dependencies: [ "XCDBLD" ]),
         Target(name: "carthage", dependencies: [ "XCDBLD", "CarthageKit" ]),
     ],
-    dependencies: [
-        .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 3, minor: 2),
-        .Package(url: "https://github.com/Carthage/ReactiveTask.git", versions: Version(0, 11, 1)..<Version(0, 11, .max)),
-        .Package(url: "https://github.com/Carthage/Commandant.git", majorVersion: 0, minor: 12),
-        .Package(url: "https://github.com/jdhealy/PrettyColors.git", majorVersion: 5),
-        .Package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", majorVersion: 1, minor: 1),
-        .Package(url: "https://github.com/mdiep/Tentacle.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/thoughtbot/Argo.git", majorVersion: 4, minor: 1),
-        .Package(url: "https://github.com/thoughtbot/Runes.git", versions: Version(4, 0, 1)..<Version(4, .max, .max)),
-    ],
+    dependencies: {
+        var deps: [Package.Dependency] = [
+            .Package(url: "https://github.com/antitypical/Result.git", versions: Version(3, 2, 1)..<Version(3, .max, .max)),
+            .Package(url: "https://github.com/Carthage/ReactiveTask.git", versions: Version(0, 11, 1)..<Version(0, 11, .max)),
+            .Package(url: "https://github.com/Carthage/Commandant.git", majorVersion: 0, minor: 12),
+            .Package(url: "https://github.com/jdhealy/PrettyColors.git", majorVersion: 5),
+            .Package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", majorVersion: 1, minor: 1),
+            .Package(url: "https://github.com/mdiep/Tentacle.git", majorVersion: 0, minor: 7),
+            .Package(url: "https://github.com/thoughtbot/Argo.git", majorVersion: 4, minor: 1),
+            .Package(url: "https://github.com/thoughtbot/Runes.git", versions: Version(4, 0, 1)..<Version(4, .max, .max)),
+        ]
+        if isSwiftPackagerManagerTest {
+            deps += [
+                .Package(url: "https://github.com/Quick/Quick.git", majorVersion: 1, minor: 1),
+                .Package(url: "https://github.com/Quick/Nimble.git", majorVersion: 6, minor: 1),
+            ]
+        }
+        return deps
+    }(),
     exclude: [
         "Source/CarthageIntegration",
         "Source/Scripts",
