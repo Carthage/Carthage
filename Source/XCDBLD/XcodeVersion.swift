f2828b47cddf17a863a8352b0e4307322ca03786
Sho Ikeda <suicaicoca@gmail.com>
2017-08-08 19:46:00 +0900

    Refactor XcodeVersion

--- Source/XCDBLD/XcodeVersion.swift
+++ Source/XCDBLD/XcodeVersion.swift
@@ -11,6 +11,7 @@ import ReactiveTask
 	}
 #endif
 
+/// Represents version and build version of an Xcode.
 public struct XcodeVersion {
 	public let version: String
 	public let buildVersion: String
@@ -20,24 +21,33 @@ public struct XcodeVersion {
 		self.buildVersion = buildVersion
 	}
 
+	internal init?(xcodebuildOutput: String) {
+		let range = NSRange(location: 0, length: xcodebuildOutput.utf16.count)
+		guard let match = XcodeVersion.regex.firstMatch(in: xcodebuildOutput, range: range) else {
+			return nil
+		}
+
+		let nsString = xcodebuildOutput as NSString
+		let version = nsString.substring(with: match.range(at: 1))
+		let buildVersion = nsString.substring(with: match.range(at: 2))
+
+		self.init(version: version, buildVersion: buildVersion)
+	}
+
+	// swiftlint:disable next force_try
+	private static let regex = try! NSRegularExpression(pattern: "Xcode ([0-9.]+)\\nBuild version (.+)")
+
 	public static func make() -> XcodeVersion? {
 		let task = Task("/usr/bin/xcrun", arguments: ["xcodebuild", "-version"])
 		return task.launch()
 			.ignoreTaskData()
 			.map { String(data: $0, encoding: .utf8)! }
-			.flatMap(.concat) { input -> SignalProducer<XcodeVersion, TaskError> in
-				// swiftlint:disable next force_try
-				let regex = try! NSRegularExpression(pattern: "Xcode ([0-9.]+)\\nBuild version (.+)")
-				guard let match = regex.firstMatch(in: input, range: NSRange(location: 0, length: input.utf16.count)) else {
+			.flatMap(.concat) { output -> SignalProducer<XcodeVersion, TaskError> in
+				if let xcodeVersion = XcodeVersion(xcodebuildOutput: output) {
+					return .init(value: xcodeVersion)
+				} else {
 					return .empty
 				}
-
-				let nsString = input as NSString
-				let version = nsString.substring(with: match.range(at: 1))
-				let buildVersion = nsString.substring(with: match.range(at: 2))
-
-				let xcodeVersion = XcodeVersion(version: version, buildVersion: buildVersion)
-				return SignalProducer(value: xcodeVersion)
 			}
 			.single()?.value
 	}
