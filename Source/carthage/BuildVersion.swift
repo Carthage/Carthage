507069e8b8f36a353d7dbd8d3b93205df4c7fb08
sanhuazhang <sanhuazhang@tencent.com>
2018-01-03 11:21:20 +0800

    Cached lastest remote version to avoid blocked fetching in poor network environment

--- Source/carthage/BuildVersion.swift
+++ Source/carthage/BuildVersion.swift
@@ -5,8 +5,43 @@ import ReactiveTask
 import Result
 import Tentacle
 
+struct ExpirableVersion: Codable {
+	let version: SemanticVersion?
+	let expiredDate: Date
+
+	init(version: SemanticVersion?, shelfLife: TimeInterval) {
+		self.version = version
+		self.expiredDate = Date(timeIntervalSinceNow: shelfLife)
+	}
+
+	init?(fromJSONData data: Data?) {
+		guard let wrappedData = data, let wrapped = try? JSONDecoder().decode(ExpirableVersion.self, from: wrappedData) else {
+			return nil
+		}
+		self = wrapped
+	}
+
+	func encodedJSONData() -> Data? {
+		return try? JSONEncoder().encode(self)
+	}
+
+	var isExpired: Bool {
+		return Date().compare(expiredDate) == ComparisonResult.orderedDescending
+	}
+}
+
 /// The latest online version as a SemanticVersion object.
 public func remoteVersion() -> SemanticVersion? {
+	let userDefaults = UserDefaults.standard
+	let cachedKey = "latestCarthageRemoteVersion"
+
+	let expirableVersionData = userDefaults.data(forKey: cachedKey)
+	let expirableVersion = ExpirableVersion(fromJSONData: expirableVersionData)
+
+	guard expirableVersion == nil || expirableVersion!.isExpired else {
+		return expirableVersion!.version
+	}
+
 	let latestRemoteVersion = Client(.dotCom)
 		.execute(Repository(owner: "Carthage", name: "Carthage").releases, perPage: 2)
 		.map { _, releases in
@@ -19,5 +54,11 @@ public func remoteVersion() -> SemanticVersion? {
 		.timeout(after: 0.5, raising: CarthageError.gitHubAPITimeout, on: QueueScheduler.main)
 		.first()
 
-	return latestRemoteVersion?.value
+	// Expired after one day
+	let latestExpirableVersion = ExpirableVersion(version: latestRemoteVersion?.value,
+	                                              shelfLife: 24 * 60 * 60)
+	if let encodedData = latestExpirableVersion.encodedJSONData() {
+		userDefaults.set(encodedData, forKey: cachedKey)
+	}
+	return latestExpirableVersion.version
 }
