19e584623e548de68f424ea5c5dc4c081d750112
Matt Diephouse <matt@diephouse.com>
2018-01-29 08:57:36 -0500

    0.28.0

--- Source/CarthageKit/CarthageKitVersion.swift
+++ Source/CarthageKit/CarthageKitVersion.swift
@@ -2,5 +2,5 @@
 public struct CarthageKitVersion {
 	public let value: SemanticVersion
 
-	public static let current = CarthageKitVersion(value: SemanticVersion(major: 0, minor: 27, patch: 0))
+	public static let current = CarthageKitVersion(value: SemanticVersion(major: 0, minor: 28, patch: 0))
 }
