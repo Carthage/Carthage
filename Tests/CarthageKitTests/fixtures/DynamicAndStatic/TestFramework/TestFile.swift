7061cd09484213c0f516b0f7b2b724390fd64c31
Nikolay Ischuk <ischuknn@gmail.com>
2018-04-07 03:59:49 +0300

    Add static and dynamic test framework

---
+++ Tests/CarthageKitTests/fixtures/DynamicAndStatic/TestFramework/TestFile.swift
@@ -0,0 +1,12 @@
+//
+//  TestFile.swift
+//  TestFramework
+//
+//  Created by Nikolay Ischuk on 07.04.2018.
+//
+
+import Foundation
+
+func testFunction() {
+    print("Hello")
+}
---
+++ Tests/CarthageKitTests/fixtures/DynamicAndStatic/TestFramework/TestFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestFramework.h
+//  TestFramework
+//
+//  Created by Stephen Marquis on 2/22/17.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TestFramework.
+FOUNDATION_EXPORT double TestFrameworkVersionNumber;
+
+//! Project version string for TestFramework.
+FOUNDATION_EXPORT const unsigned char TestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFramework/PublicHeader.h>
+
+
