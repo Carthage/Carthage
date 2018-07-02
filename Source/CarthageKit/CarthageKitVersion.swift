49c7901f851384a358bac2e5d2333cabc4f91900
Tommaso Piazza <tommaso.piazza@gmail.com>
2018-07-02 15:25:17 +0200

    Bump version to 0.30.1

--- Source/CarthageKit/CarthageKitVersion.swift
+++ Source/CarthageKit/CarthageKitVersion.swift
@@ -2,5 +2,5 @@
 public struct CarthageKitVersion {
 	public let value: SemanticVersion
 
-	public static let current = CarthageKitVersion(value: SemanticVersion(major: 0, minor: 30, patch: 0))
+	public static let current = CarthageKitVersion(value: SemanticVersion(major: 0, minor: 30, patch: 1))
 }
