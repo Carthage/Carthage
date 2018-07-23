5c72cefbf3f6dd259c8527645c228af39dafd1dd
Jason Boyle <jasonboyle@gmail.com>
2018-07-23 16:33:02 -0700

    Clarify verify command description

--- Source/carthage/Verify.swift
+++ Source/carthage/Verify.swift
@@ -7,11 +7,13 @@ import Result
 /// Type that encapsulates the configuration and evaluation of the `verify` subcommand.
 public struct VerifyCommand: CommandProtocol {
 	public let verb = "verify"
-	public let function = "Verify that the dependencies in Cartfile.resolved are compatible"
+	public let function = "Verify that the versions in Cartfile.resolved are compatible with the Cartfile requirements"
 
 	public func run(_ options: CheckoutCommand.Options) -> Result<(), CarthageError> {
 		return options.loadProject().flatMap(.concat) { (project: Project) in
-				return project.loadResolvedCartfile().map { project.verify(resolvedCartfile: $0) }
+				return project.loadResolvedCartfile().map {
+						project.verify(resolvedCartfile: $0)
+					}
 			}
 			.on(value: { _ in
 				carthage.println("No incompatibilities found in Cartfile.resolved")
