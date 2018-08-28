6ddbc087dce3e65e61202fca928f271d0b9465dc
giginet <giginet.net@gmail.com>
2018-08-29 02:02:40 +0900

    Add specs for selectAvailableSimulator

--- Tests/CarthageKitTests/SimulatorSpec.swift
+++ Tests/CarthageKitTests/SimulatorSpec.swift
@@ -59,5 +59,37 @@ class SimulatorSpec: QuickSpec {
 				expect(devices["devices"]!["iOS 12.0"]!.count).to(equal(3))
 			}
 		}
+		
+		describe("selectAvailableSimulator(of:from:)") {
+			func loadJSON(for resource: String) -> Data {
+				let url = Bundle(for: type(of: self)).url(forResource: resource, withExtension: "json")!
+				return try! Data(contentsOf: url)
+			}
+			
+			context("when there are available simulators") {
+				it("should return the first simulator of the latest version") {
+					let data = loadJSON(for: "Devices/availables")
+					let iPhoneSimulator = selectAvailableSimulator(of: .iPhoneSimulator, from: data)!
+					expect(iPhoneSimulator.udid).to(equal(UUID(uuidString: "A52BF797-F6F8-47F1-B559-68B66B553B23")!))
+					expect(iPhoneSimulator.availability).to(equal(.available))
+					expect(iPhoneSimulator.name).to(equal("iPhone 5s"))
+					
+					let watchSimulator = selectAvailableSimulator(of: .watchSimulator, from: data)!
+					expect(watchSimulator.udid).to(equal(UUID(uuidString: "290C3D57-0FF0-407F-B33C-F1A55EA44019")!))
+					expect(watchSimulator.availability).to(equal(.available))
+					expect(watchSimulator.name).to(equal("Apple Watch - 38mm"))
+					
+					let tvSimulator = selectAvailableSimulator(of: .tvSimulator, from: data)
+					expect(tvSimulator).to(beNil())
+				}
+			}
+			
+			context("when there is no available simulator") {
+				it("should return nil") {
+					let data = loadJSON(for: "Devices/unavailable")
+					expect(selectAvailableSimulator(of: .watchSimulator, from: data)).to(beNil())
+				}
+			}
+		}
 	}
 }
