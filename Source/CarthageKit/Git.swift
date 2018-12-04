19e5e558a48d7ce0a0913a4379a5e391acf763b7
Matt Diephouse <matt@diephouse.com>
2018-12-04 17:20:58 -0500

    Remove existing items before git cloning submodules (#2654)
    This fixes an error if you try to switch to using submodules.

--- Source/CarthageKit/Git.swift
+++ Source/CarthageKit/Git.swift
@@ -498,6 +498,14 @@ public func addSubmoduleToRepository(_ repositoryFileURL: URL, _ submodule: Subm
 					.then(launchGitTask([ "add", "--force", submodule.path ], repositoryFileURL: repositoryFileURL))
 					.then(SignalProducer<(), CarthageError>.empty)
 			} else {
+				// `git clone` will fail if there's an existing file at that path, so try to remove
+				// anything that's currently there. If this fails, then we're no worse off.
+				// This can happen if you first do `carthage checkout` and then try it again with
+				// `--use-submodules`, e.g.
+				if FileManager.default.fileExists(atPath: submoduleDirectoryURL.path) {
+					try? FileManager.default.removeItem(at: submoduleDirectoryURL)
+				}
+
 				let addSubmodule = launchGitTask(
 						["submodule", "--quiet", "add", "--force", "--name", submodule.name, "--", submodule.url.urlString, submodule.path],
 						repositoryFileURL: repositoryFileURL
