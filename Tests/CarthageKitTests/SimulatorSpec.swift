29cd24ac185baf7eb0ac8ea3abbc388304990b0e
km <597435+kmcbride@users.noreply.github.com>
2019-01-27 10:40:02 -0500

    Update simulator tests

--- Tests/CarthageKitTests/SimulatorSpec.swift
+++ Tests/CarthageKitTests/SimulatorSpec.swift
@@ -51,7 +51,7 @@ class SimulatorSpec: QuickSpec {
 				}
 			}
 			
-			context("Xcode 10.1 or above") {
+			context("Xcode 10.1") {
 				it("should be parsed") {
 					let decoder = JSONDecoder()
 					let data = loadJSON(for: "Simulators/availables-xcode101")
@@ -69,24 +69,99 @@ class SimulatorSpec: QuickSpec {
 					expect(unavailableSimulator.isAvailable).to(beFalse())
 				}
 			}
+
+			context("Xcode 10.2 beta") {
+				it("should be parsed") {
+					let decoder = JSONDecoder()
+					let data = loadJSON(for: "Simulators/availables-xcode102-beta")
+					let dictionary = try! decoder.decode([String: [String: [Simulator]]].self, from: data)
+					let devices = dictionary["devices"]!
+
+					let simulators = devices["com.apple.CoreSimulator.SimRuntime.iOS-12-0"]!
+					expect(simulators.count).to(equal(2))
+					let simulator = simulators.first!
+					expect(simulator.udid).to(equal(UUID(uuidString: "A52BF797-F6F8-47F1-B559-68B66B553B23")!))
+					expect(simulator.isAvailable).to(beTrue())
+					expect(simulator.name).to(equal("iPhone 5s"))
+
+					let unavailableSimulator = simulators.last!
+					expect(unavailableSimulator.isAvailable).to(beFalse())
+				}
+			}
 		}
 		
 		describe("selectAvailableSimulator(of:from:)") {
 			context("when there are available simulators") {
-				it("should return the first simulator of the latest version") {
-					let data = loadJSON(for: "Simulators/availables")
-					let iPhoneSimulator = selectAvailableSimulator(of: .iPhoneSimulator, from: data)!
-					expect(iPhoneSimulator.udid).to(equal(UUID(uuidString: "A52BF797-F6F8-47F1-B559-68B66B553B23")!))
-					expect(iPhoneSimulator.isAvailable).to(beTrue())
-					expect(iPhoneSimulator.name).to(equal("iPhone 5s"))
-					
-					let watchSimulator = selectAvailableSimulator(of: .watchSimulator, from: data)!
-					expect(watchSimulator.udid).to(equal(UUID(uuidString: "290C3D57-0FF0-407F-B33C-F1A55EA44019")!))
-					expect(watchSimulator.isAvailable).to(beTrue())
-					expect(watchSimulator.name).to(equal("Apple Watch - 38mm"))
-					
-					let tvSimulator = selectAvailableSimulator(of: .tvSimulator, from: data)
-					expect(tvSimulator).to(beNil())
+				context("Xcode 10.0 or lower") {
+					it("should return the first simulator of the latest version") {
+						let data = loadJSON(for: "Simulators/availables")
+						let iPhoneSimulator = selectAvailableSimulator(of: .iPhoneSimulator, from: data)!
+						expect(iPhoneSimulator.udid).to(equal(UUID(uuidString: "A52BF797-F6F8-47F1-B559-68B66B553B23")!))
+						expect(iPhoneSimulator.isAvailable).to(beTrue())
+						expect(iPhoneSimulator.name).to(equal("iPhone 5s"))
+
+						let watchSimulator = selectAvailableSimulator(of: .watchSimulator, from: data)!
+						expect(watchSimulator.udid).to(equal(UUID(uuidString: "290C3D57-0FF0-407F-B33C-F1A55EA44019")!))
+						expect(watchSimulator.isAvailable).to(beTrue())
+						expect(watchSimulator.name).to(equal("Apple Watch - 38mm"))
+
+						let tvSimulator = selectAvailableSimulator(of: .tvSimulator, from: data)
+						expect(tvSimulator).to(beNil())
+					}
+				}
+
+				context("Xcode 10.1 beta") {
+					it("should return the first simulator of the latest version") {
+						let data = loadJSON(for: "Simulators/availables-xcode101-beta")
+						let iPhoneSimulator = selectAvailableSimulator(of: .iPhoneSimulator, from: data)!
+						expect(iPhoneSimulator.udid).to(equal(UUID(uuidString: "A52BF797-F6F8-47F1-B559-68B66B553B23")!))
+						expect(iPhoneSimulator.isAvailable).to(beTrue())
+						expect(iPhoneSimulator.name).to(equal("iPhone 5s"))
+
+						let watchSimulator = selectAvailableSimulator(of: .watchSimulator, from: data)!
+						expect(watchSimulator.udid).to(equal(UUID(uuidString: "290C3D57-0FF0-407F-B33C-F1A55EA44019")!))
+						expect(watchSimulator.isAvailable).to(beTrue())
+						expect(watchSimulator.name).to(equal("Apple Watch - 38mm"))
+
+						let tvSimulator = selectAvailableSimulator(of: .tvSimulator, from: data)
+						expect(tvSimulator).to(beNil())
+					}
+				}
+
+				context("Xcode 10.1") {
+					it("should return the first simulator of the latest version") {
+						let data = loadJSON(for: "Simulators/availables-xcode101")
+						let iPhoneSimulator = selectAvailableSimulator(of: .iPhoneSimulator, from: data)!
+						expect(iPhoneSimulator.udid).to(equal(UUID(uuidString: "A52BF797-F6F8-47F1-B559-68B66B553B23")!))
+						expect(iPhoneSimulator.isAvailable).to(beTrue())
+						expect(iPhoneSimulator.name).to(equal("iPhone 5s"))
+
+						let watchSimulator = selectAvailableSimulator(of: .watchSimulator, from: data)!
+						expect(watchSimulator.udid).to(equal(UUID(uuidString: "290C3D57-0FF0-407F-B33C-F1A55EA44019")!))
+						expect(watchSimulator.isAvailable).to(beTrue())
+						expect(watchSimulator.name).to(equal("Apple Watch - 38mm"))
+
+						let tvSimulator = selectAvailableSimulator(of: .tvSimulator, from: data)
+						expect(tvSimulator).to(beNil())
+					}
+				}
+
+				context("Xcode 10.2 beta") {
+					it("should return the first simulator of the latest version") {
+						let data = loadJSON(for: "Simulators/availables-xcode102-beta")
+						let iPhoneSimulator = selectAvailableSimulator(of: .iPhoneSimulator, from: data)!
+						expect(iPhoneSimulator.udid).to(equal(UUID(uuidString: "A52BF797-F6F8-47F1-B559-68B66B553B23")!))
+						expect(iPhoneSimulator.isAvailable).to(beTrue())
+						expect(iPhoneSimulator.name).to(equal("iPhone 5s"))
+
+						let watchSimulator = selectAvailableSimulator(of: .watchSimulator, from: data)!
+						expect(watchSimulator.udid).to(equal(UUID(uuidString: "290C3D57-0FF0-407F-B33C-F1A55EA44019")!))
+						expect(watchSimulator.isAvailable).to(beTrue())
+						expect(watchSimulator.name).to(equal("Apple Watch - 38mm"))
+
+						let tvSimulator = selectAvailableSimulator(of: .tvSimulator, from: data)
+						expect(tvSimulator).to(beNil())
+					}
 				}
 			}
 			
