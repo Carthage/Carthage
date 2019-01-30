5e0e22ad8af88c02dbf5e5509cc519447e0fc768
David Brunow <david@shep.dog>
2019-01-29 19:39:56 -0600

    Fix issue where the latest OS version can be set to a simulator version that is unavailable.

--- Source/CarthageKit/Simulator.swift
+++ Source/CarthageKit/Simulator.swift
@@ -48,7 +48,9 @@ internal func selectAvailableSimulator(of sdk: SDK, from data: Data) -> Simulato
 		guard let platformVersion = parsePlatformVersion(for: platformName, from: entry.key) else { return }
 		result[platformVersion] = entry.value
 	}
-	let allTargetSimulators = devices.reduce(into: [:], reducePlatformNames)
+	let allTargetSimulators = devices.filter({ _, devices in 
+		return devices.filter({ $0.isAvailable }).count > 0
+		}).reduce(into: [:], reducePlatformNames)
 	func sortedByVersion(_ osNames: [String]) -> [String] {
 		return osNames.sorted { lhs, rhs in
 			guard let lhsVersion = SemanticVersion.from(PinnedVersion(lhs)).value,
