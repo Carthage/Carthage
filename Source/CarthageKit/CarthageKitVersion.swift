7a92469add9dcc806402f968636fb4e69c3432b2
David Brunow <david@shep.dog>
2020-04-10 15:49:24 -0500

    Update Carthage version to match production

--- Source/CarthageKit/CarthageKitVersion.swift
+++ Source/CarthageKit/CarthageKitVersion.swift
@@ -4,5 +4,5 @@ import Foundation
 public struct CarthageKitVersion {
 	public let value: SemanticVersion
 
-	public static let current = CarthageKitVersion(value: SemanticVersion(0, 33, 0))
+	public static let current = CarthageKitVersion(value: SemanticVersion(0, 34, 0))
 }
