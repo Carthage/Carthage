5d438e5bd40f85d614ba26343cc266fb0fa664fb
J.D. Healy <jdhealy@gmail.com>
2020-07-02 07:10:26 -0400

    Reduce, using repeating flag combo, multiple invocations of architecture stripping, lessening multiple-«copy-framework»-running-simultaneously read/write issue.
    • Add nondestructive strip architectures CarthageKit function.
    • Add private `architecturesInPackage` with return type `Value` of `[String]`
     • Differentiable from the public `architecturesInPackage`, by having an added `xcrunQuery` parameter.
    • Add preferred `copyAndStripFrameworks` function and use in «copy-frameworks».
     • Keeps around `stripFramework(_:keepingArchitectures:strippingDebugSymbols:codesigningIdentity:)` to not depreciate any public API.

--- Source/CarthageKit/Xcode.swift
+++ Source/CarthageKit/Xcode.swift
@@ -78,7 +78,10 @@ internal func frameworkSwiftVersion(_ frameworkURL: URL) -> SignalProducer<Strin
 
 private func dSYMSwiftVersion(_ dSYMURL: URL) -> SignalProducer<String, SwiftVersionError> {
 	// Pick one architecture
-	guard let arch = architecturesInPackage(dSYMURL).first()?.value else {
+	guard let arch = architecturesInPackage(
+		dSYMURL,
+		xcrunQuery: ["lipo", "-info"]
+	).flatten().first()?.value else {
 		return SignalProducer(error: .unknownFrameworkSwiftVersion(message: "No architectures found in dSYM."))
 	}
 
@@ -982,6 +985,42 @@ public func buildInDirectory( // swiftlint:disable:this function_body_length
 	}
 }
 
+public func copyAndStripFramework(
+	_ source: URL,
+	target: URL,
+	validArchitectures: [String],
+	strippingDebugSymbols: Bool = true,
+	queryingCodesignIdentityWith codesignIdentityQuery: SignalProducer<String?, CarthageError> = .init(value: nil),
+	copyingSymbolMapsInto symbolMapDestinationSignal: Result<URL, CarthageError>? = nil
+) -> SignalProducer<(), CarthageError> {
+	let strippedArchitectureData = architecturesInPackage(source)
+		.flatMap(.race) { (archs: [String]) in
+			nonDestructivelyStripArchitectures(source, Set(archs).subtracting(validArchitectures))
+		}
+
+	return SignalProducer.combineLatest(copyProduct(source, target), codesignIdentityQuery, strippedArchitectureData)
+		.flatMap(.merge) { url, codesigningIdentity, strippedArchitectureData -> SignalProducer<(), CarthageError> in
+			return SignalProducer(value: strippedArchitectureData.1.relativePath)
+				.attemptMap {
+					return Result(at: target.appendingPathComponent($0)) {
+						try strippedArchitectureData.0.write(to: $0)
+					}
+				}
+				.concat(strippingDebugSymbols ? stripDebugSymbols(target) : .empty)
+				.concat(stripHeadersDirectory(target))
+				.concat(stripPrivateHeadersDirectory(target))
+				.concat(stripModulesDirectory(target))
+				.concat(codesigningIdentity.map { codesign(target, $0) } ?? .empty)
+				.concat(
+					(symbolMapDestinationSignal?.producer ?? SignalProducer.empty)
+						.flatMap(.merge) {
+							BCSymbolMapsForFramework(source).copyFileURLsIntoDirectory($0)
+						}
+						.then(SignalProducer<(), CarthageError>.empty)
+				)
+		}
+}
+
 /// Strips a framework from unexpected architectures and potentially debug symbols,
 /// optionally codesigning the result.
 public func stripFramework(
@@ -1127,6 +1166,57 @@ extension Signal where Value: TaskEventType {
 	}
 }
 
+public func nonDestructivelyStripArchitectures(_ frameworkURL: URL, _ architectures: Set<String>) -> SignalProducer<(Data, URL), CarthageError> {
+	return SignalProducer(value: frameworkURL)
+		.attemptMap(binaryURL)
+		.attemptMap {
+			let frameworkPathComponents = sequence(state: frameworkURL.absoluteURL.pathComponents.makeIterator(), next: {
+				$0.next() ?? ""
+			})
+
+			let suffix = zip(frameworkPathComponents, $0.pathComponents).drop(while: { $0 == $1 })
+
+			if suffix.contains(where: { $0.0 != "" }) {
+				return .failure(CarthageError.internalError(description: "∆∆∆"))
+			}
+
+			return Result(
+				URLComponents(string: suffix.map { $0.1 }.joined(separator: "/"))?
+					.url(relativeTo: frameworkURL.absoluteURL.appendingPathComponent("/")),
+				failWith: CarthageError.internalError(description: "∆∆∆")
+			)
+		}
+		.zip(with: FileManager.default.reactive.createTemporaryDirectoryWithTemplate("carthage-lipo-XXXXXX"))
+		.flatMap(.race) { (relativeBinaryURL: URL, tempDir: URL) -> SignalProducer<(Data, URL), CarthageError> in
+			let outputURL = URL(string: relativeBinaryURL.relativePath, relativeTo: tempDir)!
+
+			let arguments = [
+				[ relativeBinaryURL.absoluteURL.path ],
+				architectures.flatMap { [ "-remove", $0 ] },
+				[ "-output", outputURL.path ],
+			].reduce(into: ["lipo"]) { $0.append(contentsOf: $1) }
+
+			let task = Task("/usr/bin/xcrun", arguments: arguments)
+				.launch()
+				.attempt { _ in
+					try? FileManager.default.createDirectory(at: outputURL.deletingLastPathComponent(), withIntermediateDirectories: true)
+					return .success(())
+				}
+				.mapError(CarthageError.taskError)
+
+			let result: SignalProducer<(Data, URL), CarthageError> = SignalProducer(value: outputURL)
+				.attemptMap {
+					Result(at: $0, carthageError: CarthageError.readFailed) { url in
+						defer { try? FileManager.default.removeItem(at: url) }
+						return try Data(contentsOf: url)
+					}
+						.fanout(.success(relativeBinaryURL))
+				}
+
+			return task.then(result)
+		}
+}
+
 /// Strips the given architecture from a framework.
 private func stripArchitecture(_ frameworkURL: URL, _ architecture: String) -> SignalProducer<(), CarthageError> {
 	return SignalProducer<URL, CarthageError> { () -> Result<URL, CarthageError> in binaryURL(frameworkURL) }
@@ -1136,6 +1226,66 @@ private func stripArchitecture(_ frameworkURL: URL, _ architecture: String) -> S
 				.mapError(CarthageError.taskError)
 		}
 		.then(SignalProducer<(), CarthageError>.empty)
+}
+
+// Returns a signal of all architectures present in a given package.
+private func architecturesInPackage(_ packageURL: URL, xcrunQuery: [String] = ["lipo", "-info"]) -> SignalProducer<[String], CarthageError> { 
+	let binaryURLResult = binaryURL(packageURL)
+	guard let binaryURL = binaryURLResult.value else { return SignalProducer(error: binaryURLResult.error!) }
+
+	return Task("/usr/bin/xcrun", arguments: xcrunQuery + [binaryURL.path])
+		.launch()
+		.ignoreTaskData()
+		.mapError(CarthageError.taskError)
+		.map { String(data: $0, encoding: .utf8) ?? "" }
+		.attemptMap { output -> Result<[String], CarthageError> in
+			var characterSet = CharacterSet.alphanumerics
+			characterSet.insert(charactersIn: " _-")
+
+			let scanner = Scanner(string: output)
+
+			if scanner.scanString("Architectures in the fat file:", into: nil) {
+				// The output of "lipo -info PathToBinary" for fat files
+				// looks roughly like so:
+				//
+				//     Architectures in the fat file: PathToBinary are: armv7 arm64
+				//
+				var architectures: NSString?
+
+				scanner.scanString(binaryURL.path, into: nil)
+				scanner.scanString("are:", into: nil)
+				scanner.scanCharacters(from: characterSet, into: &architectures)
+
+				let components = architectures?
+					.components(separatedBy: " ")
+					.filter { !$0.isEmpty }
+
+				if let components = components {
+					return .success(components)
+				}
+			}
+
+			if scanner.scanString("Non-fat file:", into: nil) {
+				// The output of "lipo -info PathToBinary" for thin
+				// files looks roughly like so:
+				//
+				//     Non-fat file: PathToBinary is architecture: x86_64
+				//
+				var architecture: NSString?
+
+				scanner.scanString(binaryURL.path, into: nil)
+				scanner.scanString("is architecture:", into: nil)
+				scanner.scanCharacters(from: characterSet, into: &architecture)
+
+				if let architecture = architecture {
+					return .success([architecture.replacingOccurrences(of: "\0", with: "")])
+				}
+			}
+
+			// think I changed the output of the below error (which used to use packageURL.path)
+			return .failure(.invalidArchitectures(description: "Could not read architectures from \(packageURL.path)"))
+		}
+		.reduce(into: [] as [String]) { $0.append(contentsOf: $1 as [String]) }
 }
 
 /// Returns a signal of all architectures present in a given package.
