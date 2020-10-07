545be4dfee84b34c7d8d4064c5cd982f23a550cc
Tommaso Piazza <196761+tmspzz@users.noreply.github.com>
2020-10-07 23:50:09 +0200

    Fix merge

--- Source/CarthageKit/Xcode.swift
+++ Source/CarthageKit/Xcode.swift
@@ -1176,6 +1176,8 @@ public func nonDestructivelyStripArchitectures(_ frameworkURL: URL, _ architectu
 				$0.next() ?? ""
 			})
 
+			let suffix = zip(frameworkPathComponents, $0.pathComponents).drop(while: { $0 == $1 })
+
 			if suffix.contains(where: { $0.0 != "" }) {
 				return .failure(CarthageError.internalError(description: "In attempt to read NSBundle «\(frameworkURL.absoluteString)»'s binary url, could not relativize «\($0.debugDescription)» against «\(frameworkURL.absoluteString)»."))
 			}
