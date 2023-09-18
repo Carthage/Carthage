edf085cc652b9be8270720c21d2ca1b9fb12ff94
J.D. Healy <jdhealy@gmail.com>
2023-09-18 09:43:21 -0700

    Bump version to 0.39.1

--- Source/CarthageKit/CarthageKitVersion.swift
+++ Source/CarthageKit/CarthageKitVersion.swift
@@ -4,5 +4,5 @@ import Foundation
 public struct CarthageKitVersion {
 	public let value: SemanticVersion
 
-	public static let current = CarthageKitVersion(value: SemanticVersion(0, 39, 0))
+	public static let current = CarthageKitVersion(value: SemanticVersion(0, 39, 1))
 }
